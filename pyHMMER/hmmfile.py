4136933626e9ed5cca93b158c532a3ba5cbfd997
Haydn King <hjking734@gmail.com>
2012-10-19 16:56:27 +0100

    Switch to using a seperate parser object, everything broken

--- pyHMMER/hmmfile.py
+++ pyHMMER/hmmfile.py
@@ -1,6 +1,6 @@
 """Read and Write HMMER's .hmm format"""
 
-import re
+import re, itertools, collections
 
 class HMMFileException(Exception):
 	def __init__(self, errors, warnings):
@@ -66,25 +66,352 @@ ALPHABETS = {
 		'AMINO': 'ACDEFGHIKLMNPQRSTVWY',
 		}
 
+class HMMParser:
+	"""Parse a Hidden Markov Model in HMMER3's .hmm format"""
+
+	#States
+	S_default = 0
+	S_header = 1
+	S_model_me = 2
+	S_model_ie = 3
+	S_model_st = 4
+
+	#useful regexes
+	hdr_re = re.compile(r'^(?P<key>\w+)\s+(?P<value>.+)$')
+
+	def __init__(self):
+		self.lineNum = -1
+		self.errors = []
+		self.warnings = []
+
+	def read(self, f):
+		"""Read all the HMMs in the file, either a fileobject or a string"""
+
+		#if f is a string, try to open the file
+		if isinstance(f, basestring):
+			f = open(f, 'r')
+		
+		ret = []
+		self.errors = []
+		self.warnings = []
+
+		def _cleanLine(l):
+			"""remove any comments and strip whitespace"""
+			i = l.find('#')
+			if i:
+				l = l[:i]
+			return l.strip()
+
+		def consume(iterator, n):
+			'''Advance the iterator n-steps ahead. If n is none, consume
+					entirely.'''
+			collections.deque(itertools.islice(iterator, n), maxlen=0)
+
+		#read in all the lines, cleaning each one
+		lines = [_cleanLine(l) for l in f]
+
+		state = self.S_default
+
+		iterator = enumerate(lines).__iter__()
+		for self.lineNum, line in iterator:
+			#ignore blank lines
+			if not line:
+				continue
+
+			#find the start of an HMM definition
+			if state == self.S_default:
+				if not re.match(r"^HMMER3/b", line):
+					self._addError("Invalid File: file must start with \'HMMER3/b\'")
+					#can't continue
+					self._raiseErrors()
+				else:
+					#make a new model
+					hmm = HMM()
+					state = self.S_header
+					continue
+
+				#have we found the end of the HMM
+				if re.match(r'^//', line):
+					#if this isn't where it's supposed to be
+					if state != self.S_model_me:
+						#provide a helpful error
+						ltype = 'None'
+						if state == self.S_header:
+							ltype = 'header'
+						elif state == self.S_model_ie:
+							ltype = 'insert emission'
+						elif state == self.S_model_st:
+							ltype = 'state transition'
+						self._addError('Unexpected \'//\', expecting {} line'.format(ltype))
+
+					#check if we've found the advertised number of states 
+					#			nb. hmm.LENG doesn't include the begin state """
+					if len(hmm.states)-1 < hmm.LENG:
+						self._addError('Too few states in model ({:d} < {:d})'
+								.format(len(hmm.states-1), hmm.LENG))
+					if len(hmm.states)-1 > hmm.LENG:
+						self._addError('Too many states in model ({:d} > {:d})'
+								.format(len(hmm.states-1), hmm.LENG))
+					#add the HMM to the list
+					ret.append(hmm)
+					state = self.S_default
+					continue
+
+			#parse a header line
+			if state == self.S_header:
+				if re.match(r'^HMM\s', line):
+					#Parsed each header line
+					#check all the required options were present
+					for o in REQUIRED:
+						try:
+							if not getattr(hmm, o):
+								self._addError('Option \'{}\' is required'.format(o), False)
+						except AttributeError:
+							self._addError('Option \'{}\' is required'.format(o), False)
+							
+							if o == 'LENG':
+								setattr(hmm, o, 0)
+							else:
+								setattr(hmm, o, '')	
+					
+					#check if the line after next is a COMPO line
+					if re.match(r'^COMPO\s', lines[self.lineNum+2]):
+						hmm.COMPO = self._parse_prob(lines[self.lineNum+2].split()[1:], hmm.K)
+						#drop the next two lines
+						consume(iterator, 2)
+					else:
+						#otherwise drop only one line
+						consume(iterator, 1)
+					
+					hmm.states = [State(),]
+					state = self.S_model_me
+					continue
+				
+				m = self.hdr_re.match(line)
+				if not m:
+					self._addError('Invalid header line')
+					continue
+				
+				key = m.group('key').upper()
+				val = m.group('value')
+				#check if the key is valid
+				if not (key in OPTIONS):
+					self._addWarning('Ignoring unknown option \'%s\'' % key)
+					continue
+
+				#simple strings
+				if key in ['NAME', 'ACC', 'DESC', 'ALPH', 'DATE',]:
+					setattr(hmm, key, val)
+					if key == 'ALPH':
+						if hmm.ALPH.upper() in ALPHABETS:
+							hmm.SYMBOLS = ALPHABETS[hmm.ALPH.upper()]
+							hmm.K = len(hmm.SYMBOLS)
+						else:
+							self._addError('ALPH must be \'DNA\', \'RNA\' or \'AMINO\'')
+							continue
+				#integers
+				elif key in ['LENG', 'NSEQ', 'CKSUM',]:
+					try:
+						val = int(val)
+						if val < 0:
+							raise ValueError
+						setattr(hmm, key, val)
+					except ValueError:
+						self._addError('{} must be a positive integer'.format(key))
+						continue
+				#bools
+				elif key in ['RF', 'CS', 'MAP',]:
+					b = {'yes': True, 'no': False,}
+					val = val.lower()
+					if val in b:
+						setattr(hmm, key, b[val])
+					else:
+						self._addError('{} must be \'yes\' or \'no\''.format(key))
+						continue
+				#COM
+				elif key == 'COM':
+					m2 = re.match(r'(\d+)\s+(\S+)$', val)
+					if m2:
+						try:
+							hmm.COM.append( (int(m2.group(1)), m2.group(2)))
+						except ValueError:
+							hmm.COM.append( (len(self.COM)+1, m2.group(0)))
+							continue
+					else:
+						hmm.COM.append( (len(self.COM)+1, value))
+				elif key == 'EFFN':
+					try:
+						val = float(val)
+						if val < 0:
+							raise ValueError
+						hmm.EFFN = val
+					except ValueError:
+						self._addError('EFFN must be a positive real')
+						continue
+				#pairs of floats
+				elif key in ['GA', 'TC', 'NC']:
+					m2 = re.match(r'^([\d\.]+)\s+([\d\.]+);?$', val)
+					if m2:
+						try:
+							v1 = float(m2.group(1))
+							v2 = float(m2.group(2))
+							if v1 < 0 or v2 < 0:
+								raise ValueError
+							setattr(hmm, key, (v1, v2))
+						except ValueError:
+							self._addError('{} must be two positive reals'.format(key))
+							continue
+					else:
+						self._addError('{} must be two positive reals'.format(key))
+						continue
+				#STATS
+				elif key == 'STATS':
+					m2 = re.match(r'^(\w+)\s+(\w+)\s+([\d\.-]+)\s+([\d\.-]+)$', val)
+					if m2:
+						try:
+							s1 = m2.group(1).upper()
+							s2 = m2.group(2).upper()
+							f1 = float(m2.group(3))
+							f2 = float(m2.group(4))
+							if s1 == 'LOCAL' and s2 in ['MSV', 'VITERBI', 'FORWARD']:
+								hmm.STATS.append((s1, s2, f1, f2))
+							else:
+								if s1 != 'LOCAL':
+									self._addError('s1 must equal \'LOCAL\'')
+								else:
+									self._addError('s2 must be \'MSV\', \'VITERBI\' or \'FORWARD\'')
+								continue
+						except ValueError:
+							self._addError('STATS <s1> <s2> <f1> <f2>')
+							continue
+					else:
+						self._addError('STATS <s1> <s2> <f1> <f2>')
+						continue
+			
+			if state == self.S_model_me:
+				#parse the ME line
+				l = line.split()
+				
+				model_state = State()
+
+				try:
+					num = int(l[0])
+					if num != len(self.states):
+						self._addError('Expected state number %s' % len(self.states))
+				except ValueError:
+					self._addError('Node number must be a positive integer')
+
+				model_state.me = self._parse_prob(l[1:hmm.K+1], hmm.K)
+				
+				#MAP number
+				try:
+					if(l[hmm.K+1] != '-'):
+						model_state.MAP = int(l[hmm.K+1])
+					elif hmm.MAP:
+						self._addWarning('Map annotation is \'-\', even though MAP is \'yes\'')
+				except ValueError:
+					self._addError('Map Annotation must be an integer or \'-\'')
+				except IndexError:
+					self._addError('No Map annotation provided')
+				#RF annotation
+				try:
+					model_state.RF = l[hmm.K+2]
+					if len(RF) != 1:
+						self._addError('RF annotation must be a single character')
+				except IndexError:
+					self._addError('No RF annotation provided')
+				#CS annotation
+				try:
+					model_state.CS = l[hmm.K+3]
+					if len(CS_annot) != 1:
+						self._addError('CS annotation must be a single character')
+				except IndexError:
+					self._addError('No CS annotation provided')
+				#we're now expecting an IE line
+				state = self.S_model_ie
+
+			if state == self.S_model_ie:
+				model_state.ie = self._parse_prob(line.split(), hmm.K)
+				state = self.S_model_st
+
+			elif state == self.S_model_st:
+				model_state.tr = self._parse_prob(line.split(), 7)
+				#add the state to the current hmm
+				hmm.states.append(model_state)
+
+		#parsed every line in the file
+		#if there were errors, raise them. Otherwise keep quiet
+		if len(self.errors):
+			self._raiseErrors()
+
+		return ret
+	
+	def _raiseErrors(self):
+		raise HMMFileException(self.errors, self.warnings)
+
+	def _addError(self, what, show_line=True):
+		if(show_line):
+			self.errors.append((self.lineNum, what))
+		else:
+			self.errors.append((-1, what))
+
+	def _addWarning(self, what, show_line=True):
+		if(show_line):
+			self.warnings.append((self.lineNum, what))
+		else:
+			self.warnings.append((-1, what))
+
+
+	def _parse_prob(self, l, expected=-1):
+		"""Parse a list of log probabilities"""
+		ret = []
+		for v in l:
+			if str(v) == "*":
+				ret.append(v)
+			else:
+				try:
+					if float(v) < 0:
+						self._addError('log probability was less than zero (%s)' % v)
+						ret.append('*')
+					else:
+						ret.append(float(v))
+				except ValueError:
+					self._addError('probability should be a positive float (%s)' % v)
+		if expected > 0 and len(ret) != expected:
+			self._addError('expected %s floats' % expected)
+		return ret
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 class State:
 	"""A state within an HMM"""
-	def __init__(self, num, MAP_annot, CS_annot, RF_annot, 
-						match_emission, insert_emission, transition):
-		self.num = num
-		self.MAP_annot = MAP_annot
-		self.RF_annot = RF_annot
-		self.CS_annot = CS_annot
-		self.match_emission = match_emission
-		self.insert_emission = insert_emission
-		self.transition = transition
+	def __init__(self):
+		self.num = -1
+		self.MAP = -1
+		self.RF = '-'
+		self.CS = '-'
+		self.me = []
+		self.ie = []
+		self.tr = []
 
 class HMM:
 	"""A Hidden Markov Model"""
 
-	def __init__(self, fname=''):
-		self.errors = []
-		self.warnings = []
-		self.line = -1
+	def __init__(self):
+		self.NAME = ''
+		self.LENG = 0
 
 		self.states = []
 		self.COM = []
@@ -92,37 +419,7 @@ class HMM:
 		#assume these to be false if not present
 		self.RM = self.CS = self.MAP = False
 		self.K = 0
-
-		if fname:
-			self.loadFile(fname)
-	
-	def loadFile(self, fname):
-		"""load a file from disk"""
-		#open the file and read the file
-		f = open(fname, 'r')
-		self.load(f)
-		f.close()
-
-	def load(self, f):
-		"""load from a file object"""
-		lines = [line.strip() for line in f]
-
-		#Should I try to read the model
-		self._continue = True			
-
-		#parse the header section
-		self._read_hdr(lines)
 		
-		if self._continue:
-			#parse the model section
-			self._read_mdl(lines)
-
-		if not self.isValid():
-			raise HMMFileException(self.errors, self.warnings)
-		
-	def isValid(self):
-		return (len(self.errors) == 0)
-
 	def writeToFile(self, fname):
 		f = open(fname, 'w')
 		self.write(f)
@@ -204,256 +501,5 @@ class HMM:
 		#end with //
 		ret.append('      //\n')
 		return ret
-
-	def _read_hdr(self, lines):
-		"""parse the header"""
-
-
-		#have we see the HMM line?
-		seen_HMM = False
-
-		#look for the format specification
-		#first line must start with HMMER3/b
-		self._next_line(lines);
-		if not re.match(r"^HMMER3/b", lines[self.line]):
-			raise HMMFileException(["Invalid File: file must start with "
-				"\'HMMER3/b\'"], [])
-
-		r = re.compile(r'^(?P<key>\w+)\s+(?P<value>.+)$')
-		while self._next_line(lines) < len(lines):
-			if re.match(r'^HMM\s', lines[self.line]):
-				seen_HMM = True
-				break
-			
-			m = r.match(lines[self.line])
-			if not m:
-				self._addError('invalid line')
-				continue
-			
-			key = m.group('key').upper()
-			val = m.group('value')
-			#check if the key is valid
-			if not (key in OPTIONS):
-				self._addWarning('ignoring unknown option \'%s\'' % key)
-				continue
-
-			#simple strings
-			if key in ['NAME', 'ACC', 'DESC', 'ALPH', 'DATE',]:
-				setattr(self, key, val)
-				if key == 'ALPH':
-					if self.ALPH.upper() in ALPHABETS:
-						self.SYMBOLS = ALPHABETS[self.ALPH.upper()]
-						self.K = len(self.SYMBOLS)
-					else:
-						self._addError('ALPH must be \'DNA\', \'RNA\' or \'AMINO\'')
-			#integers
-			elif key in ['LENG', 'NSEQ', 'CKSUM',]:
-				try:
-					val = int(val)
-					if val < 0:
-						raise ValueError
-					setattr(self, key, val)
-				except ValueError:
-					self._addError('Must be a positive integer')
-					continue
-			#bools
-			elif key in ['RF', 'CS', 'MAP',]:
-				b = {'yes': True, 'no': False,};
-				val = val.lower()
-				if val in b:
-					setattr(self, key, b[val])
-				else:
-					self._addError('value must be \'yes\' or \'no\'')
-					continue
-			#COM
-			elif key == 'COM':
-				m2 = re.match(r'(\d+)\s+(\S+)$', val)
-				if m2:
-					try:
-						self.COM.append( (int(m2.group(1)), m2.group(2)))
-					except ValueError:
-						self.COM.append( (len(self.COM)+1, m2.group(0)))
-						continue
-				else:
-					self.COM.append( (len(self.COM)+1, value))
-			elif key == 'EFFN':
-				try:
-					val = float(val)
-					if val < 0:
-						raise ValueError
-					self.EFFN = val
-				except ValueError:
-					self._addError('EFFN must be a positive real')
-					continue
-			#pairs of floats
-			elif key in ['GA', 'TC', 'NC']:
-				m2 = re.match(r'^([\d\.]+)\s+([\d\.]+);?$', val)
-				if m2:
-					try:
-						v1 = float(m2.group(1))
-						v2 = float(m2.group(2))
-						if v1 < 0 or v2 < 0:
-							raise ValueError
-						setattr(self, key, (v1, v2))
-					except ValueError:
-						self._addError('%s must be two positive reals' % key)
-						continue
-				else:
-					self._addError('%s must be two positive reals' % key)
-			#STATS
-			elif key == 'STATS':
-				m2 = re.match(r'^(\w+)\s+(\w+)\s+([\d\.-]+)\s+([\d\.-]+)$', val)
-				if m2:
-					try:
-						s1 = m2.group(1).upper()
-						s2 = m2.group(2).upper()
-						f1 = float(m2.group(3))
-						f2 = float(m2.group(4))
-						if s1 == 'LOCAL' and s2 in ['MSV', 'VITERBI', 'FORWARD']:
-							self.STATS.append((s1, s2, f1, f2))
-						else:
-							if s1 != 'LOCAL':
-								self._addError('s1 must equal \'LOCAL\'')
-							else:
-								self._addError('s2 must be \'MSV\', \'VITERBI\' or \'FORWARD\'')
-					except ValueError:
-						self._addError('STATS <s1> <s2> <f1> <f2>')
-						continue
-				else:
-					self._addError('STATS <s1> <s2> <f1> <f2>')
-					continue
-		
-		#Parsed each line
-		#check all the required options were present
-		for o in REQUIRED:
-			try:
-				getattr(self, o)
-			except AttributeError:
-				self._addError('Option \'%s\' is required' % o, False)
-				if o == 'LENG':
-					setattr(self, o, 0)
-				else:
-					setattr(self, o, '')
-
-		if not seen_HMM:
-			self._addError('No HMM line found', False)
-			self._continue = False
-
-	def _read_mdl(self, lines):
-		"""read the model"""
-		#ignore the line immediately after the HMM line
-		self._next_line(lines)
-		#parse the COMPO line, if it's present
-		if re.match(r'^COMPO\s', lines[self.line+1]):
-			self._next_line(lines)
-			self.COMPO = self._parse_prob(lines[self.line].split()[1:], self.K)
-
-		#variables to collect for each model
-		num = 0
-		MAP_annot = 0
-		RF_annot = '-'
-		CS_annot = '-'
-		match_emission = []
-		insert_emission = ['*' for i in range(0,self.K)]
-		transition = []
-
-		#state = 0 - expect Match Emission line or //
-		#state = 1 - expect Insert Emission line
-		#state = 2 - expect State Transision line
-		#state = 4 - we've seen //
-		state = 1
-		while self._next_line(lines) < len(lines):
-			if state == 0:
-				if lines[self.line] == '//':
-					state = 4
-					break
-				#parse the emission line
-				l = lines[self.line].split()
-				if len(l) != (self.K + 4):
-					self._addError('Malformed Match Emission line')
-					return
-				try:
-					num = int(l[0])
-					if num != len(self.states):
-						self._addError('Expected state number %s' % len(self.states))
-				except ValueError:
-					self._addError(self.parse_line, 'node number must be a positive integer')
-				match_emission = self._parse_prob(l[1:self.K+1])
-				#MAP number
-				try:
-					if(l[self.K+1] != '-'):
-						MAP_annot = int(l[self.K+1])
-					elif self.MAP:
-						self._addWarning('Map annotation is \'-\', even though MAP is \'yes\'')
-				except ValueError:
-					self._addError('Map Annotation must be an integer or \'-\'')
-				#RF annotation
-				RF_annot = l[self.K+2]
-				if len(RF_annot) != 1:
-					self._addError('RF annotation must be a single character')
-				#CS annotation
-				CS_annot = l[self.K+3]
-				if len(CS_annot) != 1:
-					self._addError('CS annotation must be a single character')
-				#we're now expecting an IE line
-				state = 1
-			
-			elif state == 1:
-				insert_emission = self._parse_prob(lines[self.line].split(), self.K)
-				state = 2
-
-			elif state == 2:
-				transition = self._parse_prob(lines[self.line].split(), 7)
-				state = 0
-				#add the node
-				self.states.append(State(num, MAP_annot, CS_annot, RF_annot, 
-						match_emission, insert_emission, transition))
-
-		if len(self.states) != (self.LENG + 1):
-			self._addError('Expected %s states (incl. BEGIN) but found %s' % 
-					(self.LENG+1, len(self.states)))
-		if state != 4:
-			self._addError('No \'//\' found at end of file')
-
-	def _next_line(self, lines):
-		self.line = self.line + 1
-		try:
-			while (len(lines[self.line].strip()) == 0 
-					or lines[self.line].strip()[0] == '#'):
-				self.line = self.line + 1
-		except IndexError:
-			pass
-
-		return self.line
-
-	def _parse_prob(self, l, expected=-1):
-		ret = []
-		for v in l:
-			if str(v) == "*":
-				ret.append(v)
-			else:
-				try:
-					if float(v) < 0:
-						self._addError('log probability was less than zero (%s)' % v)
-						ret.append('*')
-					else:
-						ret.append(float(v))
-				except ValueError:
-					self._addError('probability should be a positive float (%s)' % v)
-		if expected > 0 and len(ret) != expected:
-			self._addError('expected %s floats' % expected)
-		return ret
-
-	def _addError(self, what, show_line=True):
-		if(show_line):
-			self.errors.append((self.line, what))
-		else:
-			self.errors.append((-1, what))
-
-	def _addWarning(self, what, show_line=True):
-		if(show_line):
-			self.warnings.append((self.line, what))
-		else:
-			self.warnings.append((-1, what))
 
 
