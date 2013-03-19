ddd4f3eddcf6237026424fe86e2f76d4e1bf6a08
Haydn King <hjking734@gmail.com>
2013-03-19 19:03:27 +0000

    tools also yields sequences

--- pyHMMER/tools.py
+++ pyHMMER/tools.py
@@ -9,12 +9,9 @@ import sequtils
 def getSixFrameTranslation(rec):
 	"""Return the six frame translations of SeqRecord rec"""
 	frames = ["+1", "+2", "+3", "-1", "-2", "-3",]
-	c = sequtils.sixFrameTranslation(str(rec.seq))
-	r = []
-	for i,s in enumerate(c):
-		r.append(SeqRecord(Seq(s, generic_protein),
+	for i,s in enumerate(sequtils.sixFrameTranslation(str(rec.seq))):
+		yield SeqRecord(Seq(s, generic_protein),
 			name=rec.name, id=rec.id,
 			description= "%s; frame: %s" % (rec.description, frames[i]), 
-			))
-	return r
+			)
 
